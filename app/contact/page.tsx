342552e5fbed7a46c9d5b32b3ae52d56fe005976
nepalrohan <rohannepal098@gmail.com>
2025-04-18 13:15:23 +0545

    title metada template

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata:Metadata = {
+  title:'Welcome to nextjs app',
+
+    
+  description:'hellow from my app'
+}
+
 
 const ContactPage = () => {
   return (
