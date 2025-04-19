c0b95a005aa9845acb21f964ee3867f7832b29cc
nepalrohan <rohannepal098@gmail.com>
2025-04-19 18:01:06 +0545

    navigation in parallel routing

--- app/charts/@HeatMap/page.tsx
+++ app/charts/@HeatMap/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeatMap = () => {
   return (
-    <div className='w-40 h-40 bg-green-400'>HeatMap</div>
+    <div className='w-40 h-40 bg-green-400'>HeatMap
+    
+    <button><Link href='/charts/pie-chart'>Pie-Chart</Link></button>
+    </div>
   )
 }
 
---
+++ app/charts/@HeatMap/pie-chart/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+    <button><Link href='/charts'>HeatMap</Link></button>
+
+    </div>
+  )
+}
+
+export default page
